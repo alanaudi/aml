4c792c31e48c1f587adb7049e118dc24a5feb61f
alanaudi <alanaudi.tw@gmail.com>
2020-06-03 22:52:20 +0800

    (feat): Get chunk sample

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+# Standard import
 import csv
 import os
 import json
-from typing import List, Optional
-from pydantic import BaseModel, Field, validator
 import ast
+from typing import List, Optional, Iterator
+import itertools
+
+# Third-party import
+from pydantic import BaseModel, Field, validator
 
 csvfile = os.listdir('./dataset')[1]
 
@@ -22,14 +26,6 @@ class Sample(BaseModel):
 
     @validator('news_ID')
     def str2int(cls, s):
-        """String to integer
-
-        Args:
-            s: "255"
-
-        Return:
-            int: 255
-        """
         return int(s)
 
     @validator('name', pre=True)
@@ -44,9 +40,49 @@ class Sample(BaseModel):
         return ast.literal_eval(s)
 
 
-with open(F'dataset/{csvfile}', newline='') as f:
-    data = []
-    for row in csv.DictReader(f):
-        data.append(Sample(**row))
-    print(data)
-    print(len(data))
+def get_sample(fname: str) -> Iterator[str]:
+    """ Preprocess each row to Sample
+
+    Args:
+        filename (str): dataset/tbrain_train_final_0603.csv
+
+    Yield:
+        Sample
+    """
+    with open(fname, newline='') as f:
+        for row in csv.DictReader(f):
+            yield Sample(**row)
+
+
+def get_chunk_sample(fname, chunksize=1000):
+    """ Get a chunk of samples
+
+    Args:
+        fname (str): dataset/tbrain_train_final_0603.csv
+        chunksize (int, optional): 1000
+
+    Yield:
+        Iterator
+
+    Examples:
+        >>> for i in get_chunk_sample(fname):
+        ...     print(len(list(i)))
+        1000
+        1000
+        1000
+        1000
+        1000
+        23
+    """
+    it = iter(get_sample(fname))
+    while True:
+        chunk_it = itertools.islice(it, chunksize)
+        try:
+            first = next(chunk_it)
+        except StopIteration:
+            return
+        yield itertools.chain((first,), chunk_it)
+
+
+for i in get_chunk_sample(F'dataset/{csvfile}'):
+    print(len(list(i)))
