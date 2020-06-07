a2ebd0d2740682dc1732a08d911a97bb2fe165c6
alanaudi <alanaudi.tw@gmail.com>
2020-06-08 07:49:45 +0800

    (feat): Add yaml for selctor config

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import itertools
 import os
 from pathlib import Path
 import re
+import yaml
 
 # Third-party import
 import pandas as pd
@@ -16,11 +17,17 @@ DATASET_DIR = ROOT / 'dataset'
 DOC_DIR = ROOT / 'doc'
 
 # csv dataset
-csvfile = DATASET_DIR / os.listdir(DATASET_DIR)[1]
+csvfile = DATASET_DIR / list(filter(lambda x: x.endswith('csv'), os.listdir(DATASET_DIR)))[0]
 
 # create simplified name, host name, hyperlink
 
 host_name = set([sample.host for sample in get_sample(csvfile)])
+
+def get_selector():
+    with open('selector.yaml', 'r') as f:
+        return yaml.load(f)
+
+# patch = [1982,1983,1997, 2002, 2010, 2015, 2019, 2028, 2035, 2059, 2079, 2083, 2134, 2138, 2151, 2156, 2170]
 
 # demo sample {{{
 # print(F'Class Name\thostname\texample')
@@ -31,19 +38,21 @@ host_name = set([sample.host for sample in get_sample(csvfile)])
 # }}}
 
 # crawler {{{
-for sample in itertools.islice(get_sample(csvfile), 1):
-    config = {'selector': {
-                  'title': '#content > div > div > div._2hZZ.theme-app.theme-newsdetail > main > div._uo1n > h1',
-                  'meta': '#content > div > div > div._2hZZ.theme-app.theme-newsdetail > main > div._uo1n > div._1R6L',
-                  'figure': '#content > div > div > div._2hZZ.theme-app.theme-newsdetail > main > div._1S0A > article > figure',
-                  'content': '#content > div > div > div._2hZZ.theme-app.theme-newsdetail > main > div._1S0A > article > section._82F6 > div._1UuP',
-                  },
-              'source': sample.source,
-              'hyperlink': sample.hyperlink}
 
-    nc = NewsCrawler(**config)
-    print(nc.article)
-    # with open(F'{sample.news_ID}_{nc.source}.txt', 'w') as f:
-    #     f.write(F'hyperlink: {nc.hyperlink}\n\n')
-    #     f.write(nc.article)
+for sample in get_sample(csvfile):
+    if sample.source == 'Cnyes' and sample.news_ID > 2268:
+        config = {'selector': get_selector()[sample.source],
+                  'source': sample.source,
+                  'hyperlink': sample.hyperlink}
+        fname = F'{DATASET_DIR}/raw/{sample.source}/{sample.news_ID}.txt'
+        try:
+            nc = NewsCrawler(**config)
+            with open(fname, 'w') as f:
+                f.write(F'hyperlink: {nc.hyperlink}\n\n')
+                f.write(nc.article)
+                print(F'{fname}')
+        except:
+            print(F'{sample.news_ID}')
+
 # }}}
+print(len(list(filter(lambda x: x.source == 'Cnyes', list(get_sample(csvfile))))))
