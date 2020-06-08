36e82c1612b256be60776567527f854dc555bb08
alanaudi <alanaudi.tw@gmail.com>
2020-06-08 09:59:59 +0800

    (feat): Add data analysis
    - [x] use click for better UX

--- app.py
+++ app.py
@@ -1,27 +1,35 @@
+#!/usr/bin/env python3
 # Standard import
+import sys
 import itertools
+import glob
 import os
 from pathlib import Path
 import re
+import traceback
 import yaml
 
 # Third-party import
 import pandas as pd
+import click
+from tabulate import tabulate
+from easydict import EasyDict
 
 # Local import
 from data import Sample, get_sample
 from crawler import NewsCrawler
+
 # GLOBAL
 ROOT = Path(os.getcwd())
 DATASET_DIR = ROOT / 'dataset'
 DOC_DIR = ROOT / 'doc'
 
 # csv dataset
-csvfile = DATASET_DIR / list(filter(lambda x: x.endswith('csv'), os.listdir(DATASET_DIR)))[0]
+csvfile = DATASET_DIR / glob.glob(F'{str(DATASET_DIR)}/*.csv')[0]
 
 # create simplified name, host name, hyperlink
-
 host_name = set([sample.host for sample in get_sample(csvfile)])
+
 
 def get_selector():
     with open('selector.yaml', 'r') as f:
@@ -29,30 +37,82 @@ def get_selector():
 
 # patch = [1982,1983,1997, 2002, 2010, 2015, 2019, 2028, 2035, 2059, 2079, 2083, 2134, 2138, 2151, 2156, 2170]
 
-# demo sample {{{
-# print(F'Class Name\thostname\texample')
-# for sample in itertools.islice(get_sample(csvfile), 3):
-#     if sample.host in host_name:
-#         print(F'{sample.source}\t{sample.host}\t{sample.hyperlink}')
-#         host_name.remove(sample.host)
+
+# Global settings {{{
+_global_options = [
+    click.option('-s', '--source', 'source', default='Cnyes', help='Source name from 39 news source'),
+]
+
+def global_options(func):
+    for option in reversed(_global_options):
+        func = option(func)
+    return func
 # }}}
 
-# crawler {{{
+@click.group()
+@global_options
+def main(**kwargs):
+    pass
 
-for sample in get_sample(csvfile):
-    if sample.source == 'Cnyes' and sample.news_ID > 2268:
-        config = {'selector': get_selector()[sample.source],
-                  'source': sample.source,
-                  'hyperlink': sample.hyperlink}
-        fname = F'{DATASET_DIR}/raw/{sample.source}/{sample.news_ID}.txt'
-        try:
-            nc = NewsCrawler(**config)
-            with open(fname, 'w') as f:
-                f.write(F'hyperlink: {nc.hyperlink}\n\n')
-                f.write(nc.article)
-                print(F'{fname}')
-        except:
-            print(F'{sample.news_ID}')
+@main.command()
+@global_options
+def crawl(**kwargs): # {{{
+    # Print argument, parameter, option {{{
+    print(tabulate(list(kwargs.items()), headers=['Name', 'Value'], tablefmt='orgtbl'))
+    args = EasyDict(kwargs)
+    # }}}
 
-# }}}
-print(len(list(filter(lambda x: x.source == 'Cnyes', list(get_sample(csvfile))))))
+    for sample in get_sample(csvfile):
+        if sample.source == 'Cnyes' and sample.news_ID == 231:
+            config = {'selector': get_selector()[sample.source],
+                      'source': sample.source,
+                      'hyperlink': sample.hyperlink}
+            fname = F'{DATASET_DIR}/{sample.news_ID}.txt'
+            try:
+                nc = NewsCrawler(**config)
+                with open(fname, 'w') as f:
+                    f.write(F'hyperlink: {nc.hyperlink}\n\n')
+                    f.write(nc.article)
+                    print(F'{fname}')
+            except Exception as e:
+                _type = e.__class__.__name__
+                _detail = e.args[0]
+                print(F'{sample.news_ID}')
+                cl, exc, tb = sys.exc_info()
+                last_call_stack = traceback.extract_tb(tb)[-1]
+                _fname = last_call_stack[0]
+                _line = last_call_stack[1]
+                _func_name = last_call_stack[2]
+                print(F'{_fname}: line {_line}, def {_func_name}() -> {_type}, {_detail}')
+    # }}}
+
+
+@main.command()
+@global_options
+def info(**kwargs): # {{{
+    """Get data analysis (source.tsv)
+    \f
+
+    Output:
+        doc/source.tsv
+    """
+    # Print argument, parameter, option {{{
+    print(tabulate(list(kwargs.items()), headers=['Name', 'Value'], tablefmt='orgtbl'))
+    args = EasyDict(kwargs)
+    # }}}
+    # create source infomation
+    sort = []
+    print(F'Amount\tClass Name\thostname\texample')
+    for sample in get_sample(csvfile):
+        if sample.host in host_name:
+            # print(F'{sample.source}\t{sample.host}\t{sample.hyperlink}')
+            nums = len(list(filter(lambda x: x.source == sample.source, get_sample(csvfile))))
+            sort.append(F'{nums}\t{sample.source}\t{sample.host}\t{sample.hyperlink}')
+            host_name.remove(sample.host)
+    for e in sorted(sort, key=lambda x: x.split('\t')[1]):
+        print(e)
+    # }}}
+
+
+if '__main__' == __name__:
+    main()
