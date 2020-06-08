f7ef10b961253af21f4ee84f9ddfeb80d06f12da
alanaudi <alanaudi.tw@gmail.com>
2020-06-08 12:53:15 +0800

    (feat): Add color config

---
+++ color.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+
+class Color:
+    """ ANSI Color Code """
+    WHITE = '\033[1;37m'
+    BLACK = '\033[0;30m'
+    RED = '\033[0;31m'
+    YELLOW = '\033[1;33m'
+    GRAY = '\033[0;37m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    WARNING = '\033[93m'
+
+    HEADER = '\033[95m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
